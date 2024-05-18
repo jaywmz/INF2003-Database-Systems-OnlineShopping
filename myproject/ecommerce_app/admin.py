4613c5803e4d6bc641b38184eb44939cd158a583
Jiang Weimin <lichtwm@hotmail.com>
2024-05-18 15:26:38 +0800

    feat: register models on admin page

--- myproject/ecommerce_app/admin.py
+++ myproject/ecommerce_app/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import User, Seller, Customer, Geolocation, Product, CartItem, Order, OrderPayment, OrderReview, OrderItem, Session
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Seller)
+admin.site.register(Customer)
+admin.site.register(Geolocation)
+admin.site.register(Product)
+admin.site.register(CartItem)
+admin.site.register(Order)
+admin.site.register(OrderPayment)
+admin.site.register(OrderReview)
+admin.site.register(OrderItem)
+admin.site.register(Session)
